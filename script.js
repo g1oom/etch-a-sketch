70067aa1054e2401b6357093095df0db7acbe8fd
g1oom <albertowj2001@gmail.com>
2020-11-11 17:17:42 +0800

    Add script.js and modified index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+
+function createGrid(gridNumber) {
+    for (let i = 1; i <= (gridNumber ** 2); i++) {
+        let item = document.createElement('div');
+        item.setAttribute('id', 'item');
+        
+        item.addEventListener('mouseover', () => item.classList.add('hover'));
+        item.addEventListener('mouseout', () => item.classList.remove('hover'));
+    
+        container.appendChild(item);
+    }
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+}
+
+
+createGrid(16);
+const resetButton = document.createElement('button');
