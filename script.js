5a5105a0c41da56626e9d16fbaaa78f6f7cb171e
g1oom <albertowj2001@gmail.com>
2021-01-01 14:13:40 +0800

    Added reset function to button

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const container = document.querySelector('.container');
 function createGrid(gridNumber) {
     for (let i = 1; i <= (gridNumber ** 2); i++) {
         let item = document.createElement('div');
-        item.setAttribute('id', 'item');
+        item.setAttribute('class', 'item');
         
         item.addEventListener('mouseover', () => item.classList.add('hover'));
-        /*item.addEventListener('mouseout', () => item.classList.remove('hover'));*/
     
         container.appendChild(item);
     }
@@ -14,8 +13,19 @@ function createGrid(gridNumber) {
     container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
 }
 
+function reset() {
+    const items = document.querySelectorAll('.item');
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove('hover');
+    }
+}
+
 
 createGrid(16);
 const resetButton = document.createElement('button');
 resetButton.classList.add('button');
+resetButton.textContent = 'Reset';
 container.appendChild(resetButton);
+
+resetButton.onclick = function() {reset()};
