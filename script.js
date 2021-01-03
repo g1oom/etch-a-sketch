63fca5b3b3ef77ffeebe0a742f4905dba833275d
g1oom <albertowj2001@gmail.com>
2021-01-03 23:25:11 +0800

    Added prompt and create new grid, randomly generate colours, and darken after each pass functions

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-const container = document.querySelector('.container');
-
 function createGrid(gridNumber) {
     for (let i = 1; i <= (gridNumber ** 2); i++) {
         let item = document.createElement('div');
         item.setAttribute('class', 'item');
-        
-        item.addEventListener('mouseover', () => item.classList.add('hover'));
-    
+        item.addEventListener('mouseover', function(){changeColour(item)}, false);
         container.appendChild(item);
     }
     container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
 }
 
-function reset() {
-    const items = document.querySelectorAll('.item');
-
-    for (let i = 0; i < items.length; i++) {
-        items[i].classList.remove('hover');
+function resetNewGrid() {
+    /* prompt */
+    let newGridNumber = prompt("Enter number of squares per side (1~100): ");
+    while (isNaN(parseInt(newGridNumber)) || !Number.isInteger(parseFloat(newGridNumber)) || 
+    parseInt(newGridNumber) < 1 || parseInt(newGridNumber) > 100) {
+        newGridNumber = prompt("Enter number of squares per side (1~100): ", "1~100");
     }
+    newGridNumber = parseInt(newGridNumber);
+
+    /* remove old grid */
+    const items = document.querySelectorAll('.item');
+    items.forEach(e => e.remove());
+
+    /* create new grid */
+    createGrid(newGridNumber);
 }
 
+function changeColour(item) {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    item.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+    const currentBrightness =  parseFloat(window.getComputedStyle(item).getPropertyValue('filter').match(/\d/g).join('.'));
+    let newBrightness = currentBrightness;
+    if (currentBrightness > 0) {
+        newBrightness = currentBrightness - 0.1;
+    }
+    item.style.filter = `brightness(${newBrightness})`;
+}
+
+const container = document.querySelector('.container');
+const body = document.body;
 
 createGrid(16);
+
 const resetButton = document.createElement('button');
 resetButton.classList.add('button');
 resetButton.textContent = 'Reset';
-container.appendChild(resetButton);
+body.insertBefore(resetButton, container);
 
-resetButton.onclick = function() {reset()};
+resetButton.addEventListener('click', resetNewGrid, false);
